33c64a7d2f27008b7ad45dd046848202db116543
jacobnisnevich <jacob.nisnevich@gmail.com>
2014-08-23 14:19:31 -0700

    Revert "reverted previous change"
    This reverts commit 9c69cc4feb7a46b1e70e981571d51b99082fd3a7.

--- scripts/script.js
+++ scripts/script.js
@@ -138,7 +138,7 @@ function init() {
         moveBall(dir);
 
         if (hitTest(ball.position.x, ball.position.y, ball.width, ball.height, platform.position.x, platform.position.y, platform.width, platform.height)) {
-            flip(dir);
+            dir = "up";
         }
 
         createBowlingPin();
