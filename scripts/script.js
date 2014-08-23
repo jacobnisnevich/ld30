68c6770882c59a29757271b457038803c399ce86
jacobnisnevich <jacob.nisnevich@gmail.com>
2014-08-22 19:01:59 -0700

    cleaned up initializiation

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,7 @@
-window.onload = function() {
-    // create an new instance of a pixi stage
+function init() {
     var stage = new PIXI.Stage(0x66FF99);
-
-    // create a renderer instance.
-    var renderer = PIXI.autoDetectRenderer(800, 600);
-
-    // add the renderer view element to the DOM
-    document.body.appendChild(renderer.view);
-
-    requestAnimFrame( animate );
-
-    // create a texture from an image path
-    var texture = PIXI.Texture.fromImage("images/bunny.png");
-    // create a new Sprite using the texture
-    var bunny = new PIXI.Sprite(texture);
-
-    // center the sprites anchor point
-    bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
-
-    // move the sprite t the center of the screen
-    bunny.position.x = 200;
-    bunny.position.y = 150;
-
-    stage.addChild(bunny);
-
-    function animate() {
-
-        requestAnimFrame( animate );
-
-        // just for fun, lets rotate mr rabbit a little
-        bunny.rotation += 0.1;
-
-        // render the stage   
-        renderer.render(stage);
-    }
+    var width = document.getElementById("game-canvas").width;
+    var height = document.getElementById("game-canvas").height;
+    renderer = PIXI.autoDetectRenderer(width, height, document.getElementById("game-canvas"));
+    renderer.render(stage);
 }
