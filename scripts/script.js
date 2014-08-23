42a88ce56129a407d2497deb7c7d14a751fa9ae0
jacobnisnevich <jacob.nisnevich@gmail.com>
2014-08-22 22:20:21 -0700

    modified tiling background texture

--- scripts/script.js
+++ scripts/script.js
@@ -6,14 +6,29 @@ function init() {
     renderer.render(stage);
 
     var farTexture = PIXI.Texture.fromImage("images/background_far.png");
-    far = new PIXI.Sprite(farTexture);
+    far = new PIXI.TilingSprite(farTexture, 512, 256);
     far.position.x = 0;
     far.position.y = 0;
+    far.tilePosition.x = 0;
+    far.tilePosition.y = 0;
     stage.addChild(far);
 
     var midTexture = PIXI.Texture.fromImage("images/background_near.png");
-    mid = new PIXI.Sprite(midTexture);
+    mid = new PIXI.TilingSprite(midTexture, 512, 256);
     mid.position.x = 0;
     mid.position.y = 0;
+    mid.tilePosition.x = 0;
+    mid.tilePosition.y = 0;
     stage.addChild(mid);
+
+    requestAnimFrame(update);
+}
+
+function update() {
+  far.tilePosition.x -= 0.128;
+  mid.tilePosition.x -= 0.64;
+
+  renderer.render(stage);
+
+  requestAnimFrame(update);
 }
