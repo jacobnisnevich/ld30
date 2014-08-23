9c81c4613fd35e3739cd9cb5106fb570308e7d0a
jacobnisnevich <jacob.nisnevich@gmail.com>
2014-08-22 18:54:45 -0700

    javascript now functional

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,38 @@
-// create an new instance of a pixi stage
-var stage = new PIXI.Stage(0x66FF99);
+window.onload = function() {
+    // create an new instance of a pixi stage
+    var stage = new PIXI.Stage(0x66FF99);
 
-// create a renderer instance.
-var renderer = PIXI.autoDetectRenderer(400, 300);
+    // create a renderer instance.
+    var renderer = PIXI.autoDetectRenderer(800, 600);
 
-// add the renderer view element to the DOM
-document.body.appendChild(renderer.view);
-
-requestAnimFrame( animate );
-
-// create a texture from an image path
-var texture = PIXI.Texture.fromImage("images/bunny.png");
-// create a new Sprite using the texture
-var bunny = new PIXI.Sprite(texture);
-
-// center the sprites anchor point
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
-
-// move the sprite t the center of the screen
-bunny.position.x = 200;
-bunny.position.y = 150;
-
-stage.addChild(bunny);
-
-function animate() {
+    // add the renderer view element to the DOM
+    document.body.appendChild(renderer.view);
 
     requestAnimFrame( animate );
 
-    // just for fun, lets rotate mr rabbit a little
-    bunny.rotation += 0.1;
+    // create a texture from an image path
+    var texture = PIXI.Texture.fromImage("images/bunny.png");
+    // create a new Sprite using the texture
+    var bunny = new PIXI.Sprite(texture);
 
-    // render the stage   
-    renderer.render(stage);
+    // center the sprites anchor point
+    bunny.anchor.x = 0.5;
+    bunny.anchor.y = 0.5;
+
+    // move the sprite t the center of the screen
+    bunny.position.x = 200;
+    bunny.position.y = 150;
+
+    stage.addChild(bunny);
+
+    function animate() {
+
+        requestAnimFrame( animate );
+
+        // just for fun, lets rotate mr rabbit a little
+        bunny.rotation += 0.1;
+
+        // render the stage   
+        renderer.render(stage);
+    }
 }
